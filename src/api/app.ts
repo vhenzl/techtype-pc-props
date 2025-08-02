0ccddf48acef13a3c422eeaad4acf9d69f409581
Vašek Henzl <vasek.henzl@gmail.com>
2025-08-02 13:35:19 +1200

    Add "POST /nodes" API endpoint

--- src/api/app.ts
+++ src/api/app.ts
@@ -1,19 +1,34 @@
 import express from 'express';
+import { createCreateNodeCommandHandler, CreateNodeCommandSchema } from '../application/use-cases/create-node.ts';
 import { createGetNodeSubtreeQueryHandler, GetNodeSubtreeQuerySchema } from '../application/use-cases/get-node-subtree.ts';
 import { createConnection } from '../infrastructure/persistence/db.ts';
-import { withQueryValidation } from '../infrastructure/processing.ts';
-import { createGetSubtreeController } from './controllers.ts';
+import { PostgresNodeRepository } from '../infrastructure/persistence/postgres-node-repository.ts';
+import { PostgresNodePropertyRepository } from '../infrastructure/persistence/postgres-property-repository.ts';
+import { withCommandLogging, withCommandValidation, withQueryValidation } from '../infrastructure/processing.ts';
+import { createGetSubtreeController, createPostNodeController } from './controllers.ts';
 import * as middlewares from './middleware.ts';
 
 // ----- services composition -----
 const db = await createConnection();
 
+const nodeRepository = new PostgresNodeRepository(db);
+const nodePropertyRepository = new PostgresNodePropertyRepository(db);
+
 const getSubtreeQueryHandler = withQueryValidation(
   GetNodeSubtreeQuerySchema,
   createGetNodeSubtreeQueryHandler({ db }),
 );
-
+const createNodeCommandHandler = withCommandLogging(
+  withCommandValidation(
+    CreateNodeCommandSchema,
+    createCreateNodeCommandHandler({ nodeRepository, nodePropertyRepository }),
+  ),
+);
 const getSubtreeController = createGetSubtreeController(getSubtreeQueryHandler);
+const postNodeController = createPostNodeController(
+  createNodeCommandHandler,
+  getSubtreeQueryHandler,
+);
 
 // ----- express app composition -----
 const app = express();
@@ -22,6 +37,7 @@ app.use(middlewares.logger);
 app.use(middlewares.requireJsonPost);
 app.use(express.json());
 
+app.post('/nodes', postNodeController);
 app.use('/subtree/*path', getSubtreeController);
 
 app.use(middlewares.errorLogger);
--- src/api/controllers.ts
+++ src/api/controllers.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import z from 'zod';
+import type { CommandHandler } from '../application/building-blocks/command.ts';
 import type { QueryHandler } from '../application/building-blocks/query.ts';
+import { createNodeCommand, type CreateNodeCommand } from '../application/use-cases/create-node.ts';
 import { createGetNodeSubtreeQuery, type GetNodeSubtreeQuery, type GetNodeSubtreeResult } from '../application/use-cases/get-node-subtree.ts';
 
 const GetSubtreeParamsSchema = z.object({
@@ -18,5 +20,40 @@ export function createGetSubtreeController(
       data: result,
     };
     res.json(data);
+  };
+}
+
+const PostNodeBodySchema = z.object({
+  parentNodeId: z.uuid().nullable(),
+  name: z.string().trim().min(1, 'Name cannot be empty'),
+  properties: z.array(z.object({
+    name: z.string().trim().min(1, 'Property name cannot be empty'),
+    value: z.number('Property value must be a valid number'),
+  })).optional().default([]),
+});
+
+export function createPostNodeController(
+  commandHandler: CommandHandler<CreateNodeCommand, string>,
+  queryHandler: QueryHandler<GetNodeSubtreeQuery, GetNodeSubtreeResult>,
+) {
+  return async function (req: express.Request, res: express.Response) {
+    // TODO: extract validation to middleware
+    const parsed = PostNodeBodySchema.safeParse(req.body);
+    if (!parsed.success) {
+      res.status(400).json({
+        errors: parsed.error.issues.map(issue => ({
+          path: issue.path,
+          message: issue.message,
+        })),
+      });
+      return;
+    }
+    const command = createNodeCommand(parsed.data.parentNodeId, parsed.data.name, parsed.data.properties);
+    const id = await commandHandler(command);
+    const result = await queryHandler(createGetNodeSubtreeQuery({ nodeId: id }));
+    const data = {
+      data: result,
+    };
+    res.status(201).json(data);
   };
 }
